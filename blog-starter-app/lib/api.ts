4180a0df6620a5fa57ce679dfe298e7bc851099e
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2022-07-11 22:51:29 +0900

    階層構造を変更したpostを読み込めるようにする

--- blog-starter-app/lib/api.ts
+++ blog-starter-app/lib/api.ts
@@ -9,8 +9,7 @@ export function getPostSlugs() {
 }
 
 export function getPostBySlug(slug: string, fields: string[] = []) {
-  const realSlug = slug.replace(/\.md$/, '')
-  const fullPath = join(postsDirectory, `${realSlug}.md`)
+  const fullPath = join(postsDirectory, `${slug}/index.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
   const { data, content } = matter(fileContents)
 
@@ -23,7 +22,7 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
   // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
     if (field === 'slug') {
-      items[field] = realSlug
+      items[field] = slug
     }
     if (field === 'content') {
       items[field] = content
