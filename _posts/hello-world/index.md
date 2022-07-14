38dd12b1013273ab4ae28d22207735f9f8984c5e
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2022-07-14 12:55:01 +0900

    外部URLや/publicからも画像を表示できるようにする

--- _posts/hello-world/index.md
+++ _posts/hello-world/index.md
@@ -10,6 +10,7 @@ ogImage:
   url: '/assets/blog/hello-world/cover.jpg'
 ---
 
+![](https://placehold.jp/150x150.png)
 ![](cover2.jpg)
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus. Praesent elementum facilisis leo vel fringilla. Congue mauris rhoncus aenean vel. Egestas sed tempus urna et pharetra pharetra massa massa ultricies.
--- components/post-image.tsx
+++ components/post-image.tsx
@@ -1,19 +1,21 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { createElement } from 'react'
 
 type PostImageFunc = (slug: string) => React.FC<JSX.IntrinsicElements['img']>
 
-const PostImage: PostImageFunc = (slug) => ({
-  src,
-  alt,
-  title,
-}) => {
-  const image = require('../_posts/' + slug + '/' + src).default
+const PostImage: PostImageFunc = (slug) => (props) => {
+  try {
+    const { src, alt, title } = props
+    const image = require('../_posts/' + slug + '/' + src).default
+  
+    return <Image
+      src={image}
+      alt={alt}
+      title={title}
+    />
 
-  return <Image
-    src={image}
-    alt={alt}
-    title={title}
-  />
+  } catch (e) {
+    return createElement('img', props)
+  }
 }
 export default PostImage
